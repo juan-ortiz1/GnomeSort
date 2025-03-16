927f3906471fd9b90f9a757230113bb289b9a9a5
juan-ortiz1 <juandavidom4@gmail.com>
2025-03-15 22:40:38 -0500

    Add files via upload

---
+++ Gnome Sort/src/gnome/sort/GnomeSort.java
@@ -0,0 +1,65 @@
+package gnome.sort;
+
+/**
+ *
+ * @author Juan Salinas & David Ortiz
+ */
+public class GnomeSort {
+
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        int index = 0;
+        while (index < array.length) {
+            if (index == 0 || less(array[index - 1], array[index])) {
+                index++;
+            } else {
+                exch(array, index, index - 1);
+                index--;
+                show(array);
+            }
+            
+        }
+    }
+
+    private static <T extends Comparable<T>> boolean less(T a, T b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static <T> void exch(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static <T extends Comparable<T>> boolean isSorted(T[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (less(array[i], array[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static <T> void show(T[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+        Integer[] array = {34, 2, 10, -9, 3, 8, 5};
+        System.out.println("Array antes de ordenar:");
+        show(array);
+        
+        
+        
+        System.out.println("Array después de ordenar:");
+        sort(array);
+        
+        System.out.println("¿Está ordenado? " + isSorted(array));
+    }
+}    
