c993dc9b34520639ac6f0ccdd08176ed53a2d2e9
juan-ortiz1 <juandavidom4@gmail.com>
2025-03-17 14:45:59 -0500

    Add files via upload

--- Gnome Sort/src/gnome/sort/GnomeSort.java
+++ Gnome Sort/src/gnome/sort/GnomeSort.java
@@ -15,13 +15,12 @@ public class GnomeSort {
                 exch(array, index, index - 1);
                 index--;
                 show(array);
-            }
-            
+            }            
         }
     }
 
     private static <T extends Comparable<T>> boolean less(T a, T b) {
-        return a.compareTo(b) < 0;
+        return a.compareTo(b) <= 0;
     }
 
     private static <T> void exch(T[] array, int i, int j) {
@@ -31,10 +30,13 @@ public class GnomeSort {
     }
 
     private static <T extends Comparable<T>> boolean isSorted(T[] array) {
-        for (int i = 1; i < array.length; i++) {
-            if (less(array[i], array[i - 1])) {
+        int index = 0;
+        while (index < array.length) {
+            if (index == 0 || less(array[index - 1], array[index])) {
+                index++;
+            } else {
                 return false;
-            }
+            }            
         }
         return true;
     }
@@ -51,12 +53,12 @@ public class GnomeSort {
     }
 
     public static void main(String[] args) {
-        Integer[] array = {34, 2, 10, -9, 3, 8, 5};
+        Integer[] array = {34, 2, 10, -9, 3, 8, 5, 9, -3, 7, 7, 12};
         System.out.println("Array antes de ordenar:");
         show(array);
         
-        
-        
+        System.out.println("¿Está ordenado? " + isSorted(array));
+                        
         System.out.println("Array después de ordenar:");
         sort(array);
         
